8e61f2e3c8fcc6685e241a3d3dacfb64db6678cc
lsavoie <twosavoie@gmail.com>
2018-09-27 21:27:19 -0500

    first commit

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,11 +1,12 @@
 class Word < ActiveRecord::Base
 
   def self.find_anagrams(string)
+    anagrams = []
     # Convert word to an array of letters
     letters = string.split(//)
 
     # Create an array to store our anagrams
-    anagrams = []
+    combinations = []
 
     # Loop through each letter in letters
     letters.each do |letter|
@@ -14,23 +15,30 @@ class Word < ActiveRecord::Base
 
       # Create a new word by combining the letter + the remaining letters
       # Add new word to anagram array
+      anagrams << letter + remaining.join('')
       anagrams << letter + reverse_letters(remaining).join('')
     end
 
+    anagrams.each do |potential|
+      if Word.find_by_text(potential).present?
+        combinations << potential
+      end
+    end
+
     # Return anagrams array
-    anagrams
+    combinations
   end
 
   def self.reverse_letters(letters)
     # Create a new array of 2 items
     length = letters.length
-    reversed_letters = Array.new(length)
+    reverse_letters = Array.new(length)
 
     # Loop through letters and keep index
     letters.each_with_index do |letter, index|
-      reversed_letters[length - index - 1] = letter
+      reverse_letters[length - index - 1] = letter
     end
 
-    reversed_letters
+    reverse_letters
   end
 end
